37d2a6c884964a7e7bcbc193ee8d307a5bbb2d15
Shivani Agrawal <shiviagrawal.31@gmail.com>
2025-12-01 02:05:16 +0000

    mart layer table code change

---
+++ models/project2_mart/country_details_oceania.sql
@@ -0,0 +1,32 @@
+{{
+config
+({
+
+"materialized":'table',
+
+"schema": 'MART'
+})
+}}
+
+WITH country_details_oceania AS
+(
+
+SELECT
+
+*
+
+FROM
+
+{{ ref('country_details_transform') }}
+
+WHERE UPPER(COUNTRY_CONTINENT_NAME) = 'OCEANIA'
+
+)
+
+ 
+
+SELECT
+
+*
+
+FROM country_details_oceania
